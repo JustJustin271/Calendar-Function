0d73d2e98eb19525e337fe39bedab7a3b507643d
Orangesicle <orangesicle213@gmail.com>
2026-04-24 15:48:39 -0400

    Refactor Zeller function and improve output formatting

--- zellerfunction.py
+++ zellerfunction.py
@@ -1,5 +1,8 @@
 days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
+month_names = ["", "January", "February", "March", "April", "May", "June",
+               "July", "August", "September", "October", "November", "December"]
+
 class Date:
     def __init__(self, month, day, year):
         self.month = month
@@ -19,7 +22,13 @@ def zeller(date):
     k = year % 100
     j = year//100
 
-    h = (day + (13 * (month + 1))//5 + k + k//4 + j//4 - 2* j)%7
+    h = ((
+    day +
+    (13 * (month + 1))//5 +
+    k +
+    k//4 +
+    j//4 -
+    2* j) %7)
     return h
 
 def day_of_week(date):
@@ -31,10 +40,22 @@ assert day_of_week(Date(1, 1, 1970)) == "Thursday"
 assert day_of_week(Date(10, 20, 2010)) == "Wednesday"
 assert day_of_week(Date(10, 1, 2011)) == "Saturday"
 assert day_of_week(Date(5, 6, 2011)) == "Friday"
-print("All tests passed!")
+print("All tests passed!\n\n")
+
+m = int(input("Enter a month (1-12): \n"))
+d = int(input("\nEnter a day of the month (1 - 31): \n"))
+y = int(input("\nEnter a year (ie. 2026): \n"))
+
+custom_date = Date(m, d, y)
+
+day_name = day_of_week(custom_date)
+
+print(
+    f"\nYour date is: \n{day_name}, "
+    f"{month_names[custom_date.month]} {custom_date.day},"
+    f" {custom_date.year}")
 
 # This is the remake of the Zeller Function from Dr. Racket
 # It is a a whole lot simplier and doesn't need let* :D
-# Justin C.
 # Created April 17th, 2026
-# Last Edited: April 17th, 2026
+# Last Edited: April 24th, 2026
